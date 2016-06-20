3a4dc5ee2ef22ab6b505d265eab137db103bbdb7
金黎明 <jinliming2@gmail.com>
2016-06-20 19:38:18 +0800

    选项页面美化

--- js/option.js
+++ js/option.js
@@ -12,15 +12,19 @@
     labSwitch.innerHTML = chrome.i18n.getMessage("labSwitch");
     labLoop.innerHTML = chrome.i18n.getMessage("labLoop") + "(" + (time - 0) / 60000 + "min): ";
     timeSlider.value = time;
-    timeSlider.addEventListener("change", function() {
+    //实时显示
+    timeSlider.addEventListener("input", function() {
         labLoop.innerHTML = chrome.i18n.getMessage("labLoop") + "(" + (timeSlider.value - 0) / 60000 + "min): ";
+    });
+    //确认保存
+    timeSlider.addEventListener("change", function() {
         localStorage.setItem("loop", timeSlider.value);
-        chrome.runtime.sendMessage({code: 0}, function(response){
+        chrome.runtime.sendMessage({code: 0}, function(response) {
         });
     });
     switch_.addEventListener("change", function() {
         localStorage.setItem("background", switch_.checked);
-        chrome.runtime.sendMessage({code: 0}, function(response){
+        chrome.runtime.sendMessage({code: 0}, function(response) {
         });
     });
 })();
